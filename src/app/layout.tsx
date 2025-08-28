9bd95ac84a9d1d49fd12a3429912093d039c195c
ZetBe <seoheewon123@hufs.ac.kr>
2025-08-28 10:12:28 +0900

    feat: 구글 애널리틱스 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
+import Script from 'next/script'
 import './globals.css'
+
+const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_ID // ✨ 2. 환경 변수에서 ID를 가져옵니다.
+
 const geistSans = Geist({
   variable: '--font-geist-sans',
   subsets: ['latin'],
@@ -26,12 +30,25 @@ export default function RootLayout({
       <head>
         <meta name="apple-mobile-web-app-title" content="bingbing" />
       </head>
+
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         suppressHydrationWarning
       >
         {children}
       </body>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_MEASUREMENT_ID}');
+        `}
+      </Script>
     </html>
   )
 }
